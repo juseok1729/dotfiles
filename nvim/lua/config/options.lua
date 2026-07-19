cbcb7f9c60907d2dbc3527775aa20499e2f7bcbc
Juseok Oh (Peter) <51759005+juseok1729@users.noreply.github.com>
2026-07-19 19:19:56 +0900

    Feat: Decrease CursorHold wait time to 500ms
    Reduce the CursorHold wait time from 4000ms to 500ms.

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- CursorHold(자동 호버) 발동 대기시간 (기본 4000ms는 너무 김)
+vim.opt.updatetime = 500
