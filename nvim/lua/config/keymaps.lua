88a242492494a98e1e18b5ba9169bab8ef46bc65
Juseok Oh (Peter) <51759005+juseok1729@users.noreply.github.com>
2026-07-19 19:18:22 +0900

    Feat: Add rounded border to manual hover keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- K 수동 호버에도 자동 호버와 동일한 둥근 테두리 적용
+vim.keymap.set("n", "K", function()
+  vim.lsp.buf.hover({ border = "rounded" })
+end, { desc = "Hover" })
